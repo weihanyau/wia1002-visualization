fe398af2c58721fa4077a8181967a4d71008ffaf
Wei Han Yau <weihanyau@gmail.com>
2023-05-31 03:46:15 +0800

    Done Lab9Q2

--- src/scenes/Lab9/Lab9Q2.tsx
+++ src/scenes/Lab9/Lab9Q2.tsx
@@ -1,4 +1,11 @@
-import { ThreadGenerator, all, createRef, waitFor } from "@motion-canvas/core";
+import {
+  DEFAULT,
+  ThreadGenerator,
+  all,
+  createRef,
+  range,
+  waitFor,
+} from "@motion-canvas/core";
 import { makeScene2D } from "@motion-canvas/2d/lib/scenes";
 import {
   Circle,
@@ -6,13 +13,20 @@ import {
   Line,
   Node,
   Rect,
+  Txt,
 } from "@motion-canvas/2d/lib/components";
-import { CodeBlock } from "@motion-canvas/2d/lib/components/CodeBlock";
+import {
+  CodeBlock,
+  edit,
+  lines,
+} from "@motion-canvas/2d/lib/components/CodeBlock";
 
 export default makeScene2D(function* (view) {
   const stackLayoutRef = createRef<Rect>();
+  const answerLayoutRef = createRef<Rect>();
   const stackScreenRectRef = createRef<Rect>();
-  const stack: Rect[] = [];
+  const inputCodeRef = createRef<CodeBlock>();
+  const stack: { rect: Rect; codeBlock: CodeBlock; code: string }[] = [];
 
   view.add(
     <Layout
@@ -26,11 +40,19 @@ export default makeScene2D(function* (view) {
       <Layout grow={1} direction={"row"} gap={25} padding={5}>
         <Rect
           layout
+          ref={answerLayoutRef}
           stroke={"#fff"}
           lineWidth={10}
           radius={20}
           grow={1}
           maxHeight={540}
+          maxWidth={"50%"}
+          wrap={"wrap"}
+          padding={20}
+          gap={35}
+          alignItems={"baseline"}
+          alignContent={"start"}
+          direction={"column"}
         ></Rect>
         <Rect
           layout
@@ -49,17 +71,17 @@ export default makeScene2D(function* (view) {
             radius={20}
             position={[0, -150]}
             points={[
-              [-150, 0],
-              [-150, 300],
-              [150, 300],
-              [150, 0],
+              [-250, 0],
+              [-250, 300],
+              [250, 300],
+              [250, 0],
             ]}
           />
           <Rect
             ref={stackLayoutRef}
             layout={false}
             position={[0, -15]}
-            size={[250, 280]}
+            size={[450, 280]}
           />
         </Rect>
       </Layout>
@@ -73,6 +95,7 @@ export default makeScene2D(function* (view) {
         padding={[60, 30]}
       >
         <CodeBlock
+          ref={inputCodeRef}
           fontSize={36.5}
           code={`
             public static void permuteString(String beginningString, String endingString) {
@@ -89,28 +112,65 @@ export default makeScene2D(function* (view) {
     </Layout>
   );
   yield* waitFor(1);
-  yield* pushToStack(stack);
-  yield* pushToStack(stack);
-  //   yield* permuteString("", "ABC");
+  yield* pushToStack(stack, `f("", "ABCD") i: 1`);
+  yield* permuteString("", "ABCD");
 
-  function* pushToStack(stack: Rect[]): ThreadGenerator {
+  function* pushToStack(
+    stack: { rect: Rect; codeBlock: CodeBlock; code: string }[],
+    code: string
+  ): ThreadGenerator {
     const newRectRef = createRef<Rect>();
+    const newCodeBlockRef = createRef<CodeBlock>();
     stackScreenRectRef().add(
       <Rect
         ref={newRectRef}
         layout={false}
+        stroke={"#1a1a1a"}
+        lineWidth={8}
         fill={"#fff"}
-        position={[0, -15]}
-        size={[250, 50]}
-      />
+        position={[0, -200]}
+        size={[450, 60]}
+      >
+        <CodeBlock
+          ref={newCodeBlockRef}
+          position={[0, 8]}
+          theme={{
+            variable: { text: "black" },
+            stringContent: { text: "black" },
+            stringPunctuation: { text: "black" },
+            entityName: { text: "black" },
+            keyword: { text: "black" },
+            literal: { text: "black" },
+          }}
+          fontSize={40}
+          code={code}
+        />
+      </Rect>
     );
     if (stack.length == 0) {
-      yield* newRectRef().bottom(stackLayoutRef().bottom, 1);
+      yield* newRectRef().bottom(stackLayoutRef().bottom, 0.5);
     } else {
-      const lastRect = stack[stack.length - 1];
-      yield* newRectRef().bottom(lastRect.top, 1);
+      const lastRect = stack[stack.length - 1].rect;
+      yield* newRectRef().bottom(lastRect.top, 0.5);
     }
-    stack.push(newRectRef());
+    stack.push({
+      rect: newRectRef(),
+      codeBlock: newCodeBlockRef(),
+      code: code,
+    });
+  }
+
+  function* popFromStack(
+    stack: { rect: Rect; codeBlock: CodeBlock; code: string }[]
+  ): ThreadGenerator {
+    if (stack.length == 0) {
+      return;
+    } else {
+      const lastRect = stack[stack.length - 1].rect;
+      yield* lastRect.scale(0, 0.5);
+      lastRect.remove();
+      stack.pop();
+    }
   }
 
   function* permuteString(
@@ -118,16 +178,57 @@ export default makeScene2D(function* (view) {
     endingString: string
   ): ThreadGenerator {
     if (endingString.length <= 1) {
-      console.log(beginningString + endingString);
+      yield* inputCodeRef().selection(lines(1, 2), 0.5);
+      const answerTxtRef = createRef<Txt>();
+      answerLayoutRef().add(
+        <Txt
+          ref={answerTxtRef}
+          fill={"#ff7e75"}
+          text={beginningString + endingString}
+          grow={0}
+          opacity={0}
+          alignSelf={"start"}
+          height={"5%"}
+        />
+      );
+      yield* answerTxtRef().opacity(1, 1);
     } else {
       for (let i = 0; i < endingString.length; i++) {
+        yield* inputCodeRef().selection(lines(3, 7), 0.5);
         let newString =
           endingString.substring(0, i) + endingString.substring(i + 1);
+        yield* pushToStack(
+          stack,
+          `f("${
+            beginningString + endingString.charAt(i)
+          }", "${newString}") i: 1`
+        );
         yield* permuteString(
           beginningString + endingString.charAt(i),
           newString
         );
       }
+    }
+    yield* popFromStack(stack);
+    if (stack.length > 0) {
+      const topCodeBlock = stack[stack.length - 1].codeBlock;
+      const topCodeString = stack[stack.length - 1].code;
+      const lastCodeExcludingIndex = topCodeString.substring(
+        0,
+        topCodeString.length - 1
+      );
+      const lastIndex = Number(topCodeString.charAt(topCodeString.length - 1));
+      stack[stack.length - 1].code = `${lastCodeExcludingIndex}${
+        lastIndex + 1
+      }`;
+      yield* topCodeBlock.edit(1.5)`${lastCodeExcludingIndex}${edit(
+        `${lastIndex}`,
+        `${lastIndex + 1}`
+      )}`;
+      yield* all(
+        topCodeBlock.selection(DEFAULT, 0.5),
+        inputCodeRef().selection(lines(3, 7), 0.5)
+      );
     }
   }
 });
